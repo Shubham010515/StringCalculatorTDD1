7e5c23720f7e1720baa6ed0dac8f738f070cbb72
Jagdish Chandole <shubham010515@gmail.com>
2024-10-19 20:21:14 +0530

    Added main class for testing.

---
+++ src/main/java/Main.java
@@ -0,0 +1,8 @@
+public class Main {
+
+    public static void main(String[] args){
+
+        StringCalculator calculator = new StringCalculator();
+        System.out.println(calculator.add("1,7"));
+    }
+}
