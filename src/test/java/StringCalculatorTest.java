eda826615b8d7520a7812c9c3e289bd25efcd165
Jagdish Chandole <shubham010515@gmail.com>
2024-10-19 19:59:53 +0530

    Created Environment

--- src/test/java/StringCalculatorTest.java
+++ src/test/java/StringCalculatorTest.java
@@ -1,2 +1,23 @@
+
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
 public class StringCalculatorTest {
+
+    StringCalculator stringCalculator;
+
+    @BeforeEach
+    public void init() {
+        stringCalculator = new StringCalculator();
+    }
+
+    @AfterEach
+    public void destroy() {
+        stringCalculator = null;
+    }
 }
