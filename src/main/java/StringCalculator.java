0269adccdeac52fe7e7d869e24d8c39777e120a1
Jagdish Chandole <shubham010515@gmail.com>
2024-10-19 20:16:44 +0530

    Created a StringCalculator class which contains method which will return the addition of numbers

--- src/main/java/StringCalculator.java
+++ src/main/java/StringCalculator.java
@@ -1,2 +1,22 @@
 public class StringCalculator {
+
+    public int add(String numbers) {
+        if (numbers.length() < 2) {
+            if (numbers.isEmpty()) {
+                return 0;
+            } else {
+                return  Integer.parseInt(numbers);
+            }
+        } else {
+            String delimiter = ",";
+            if (numbers.matches("//(.*)\n(.*)")) {
+                delimiter = Character.toString(numbers.charAt(2));
+                numbers = numbers.substring(4);
+            }
+
+            String[] numList = numbers.split(delimiter + "|\n");
+
+            return addition(numList);
+        }
+    }
 }
